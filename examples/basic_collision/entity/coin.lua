74e028ea5250bf1daf2d1eda73489d397e8447f6
Insality <insality@gmail.com>
2024-11-16 01:46:30 +0200

    Remove panthera from collision example

--- examples/basic_collision/entity/coin.lua
+++ examples/basic_collision/entity/coin.lua
@@ -1,13 +1,7 @@
 return {
 	transform = {},
-	game_object = {
-		remove_delay = 0.5,
-	},
+	game_object = {},
 	collision = true,
 	on_collision_remove = true,
-	panthera = {
-		animation_path = require("examples.basic_collision.entity.coin_panthera"),
-		play_on_remove = "on_remove"
-	},
 	on_remove_event = "score_plus"
 }
