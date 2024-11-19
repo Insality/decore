49cb7cff683f9956f01b0165e4bb4601c5e584fb
Insality <insality@gmail.com>
2024-11-20 01:41:36 +0200

    Update

--- decore/widget/debug_panel/ui_debug_panel.lua
+++ decore/widget/debug_panel/ui_debug_panel.lua
@@ -2,6 +2,7 @@ local decore_data = require("decore.internal.decore_data")
 local properties_panel = require("druid.widget.properties_panel.properties_panel")
 
 ---@class decore.widget.debug_panel: druid.widget
+---@field properties_panel widget.properties_panel
 local M = {}
 
 local PAGES = {
@@ -9,7 +10,7 @@ local PAGES = {
 	ENTITIES = "entities", -- View list of entities
 	SYSTEMS = "systems", -- View list of systems
 	SYSTEM = "system", -- View system
-	ENTITY = "entity", -- View entity
+	TABLE = "table", -- View entity/table/module
 	ENTITY_PREFABS = "entity_prefabs", -- View entity prefabs
 }
 
@@ -17,9 +18,14 @@ function M:init()
 	self.properties_panel = self.druid:new_widget(properties_panel, "properties_panel")
 	self.button_back = self.druid:new_button("button_back", self.on_button_back)
 		:set_key_trigger("key_backspace")
+
+	self.key_lctrl = self.druid:new_hotkey({ "key_lctrl", "key_lsuper" })
+
 	self.text_prev_page = self.druid:new_text("text_prev_page")
 	gui.set_parent(self:get_node("header"), self.properties_panel:get_node("header"), true)
+
 	self.page_stack = {}
+	self.undo_stack = {}
 end
 
 
@@ -31,18 +37,196 @@ end
 
 
 function M:on_button_back()
+	local is_key_ctrl = self.key_lctrl:is_processing()
+	if is_key_ctrl then
+		-- Undo
+		local undo = self.undo_stack[#self.undo_stack]
+		if undo then
+			self:select_page(undo[1], undo[2], undo[3], true)
+			table.insert(self.page_stack, { undo[1], undo[2], undo[3] })
+			table.remove(self.undo_stack)
+		end
+
+		return
+	end
+
 	local stacks = #self.page_stack
 	if stacks <= 1 then
 		return
 	end
 
-	table.remove(self.page_stack)
+	local removed_page = table.remove(self.page_stack)
+	table.insert(self.undo_stack, removed_page)
+
 	local page = self.page_stack[#self.page_stack]
 	self:select_page(page[1], page[2], page[3], true)
 end
 
 
+---Draw main menu page
+---@param context any
+---@param page_name string
+function M:draw_page_main(context, page_name)
+	self.properties_panel:add_input("World Name", "Dreams", function(value)
+		print("World", value)
+	end)
+
+	self.properties_panel:add_button("Entities", function()
+		self:select_page(PAGES.ENTITIES, self.world.entities, "Entities")
+	end):set_text_button("Inspect")
+		:set_color("#A1D7F5")
+
+	self.properties_panel:add_button("Systems" , function()
+		self:select_page(PAGES.SYSTEMS, self.world.systems, "Systems")
+	end):set_text_button("Inspect")
+		:set_color("#E6DF9F")
+
+	self.properties_panel:add_button("Entity Prefabs" , function()
+		self:select_page(PAGES.ENTITY_PREFABS, nil, "Entity Prefabs")
+	end):set_text_button("Open")
+
+	self.properties_panel:add_left_right_selector("Number", 0, function(value)
+		print("Number", value)
+	end):set_number_type(0, 10, true)
+
+	self.properties_panel:add_left_right_selector("Array", 1, function(value)
+		print("Array", value)
+	end):set_array_type({ 1, 2, 3, 4, 5 }, true)
+
+	self.properties_panel:add_left_right_selector("Number2", 0, function(value)
+		print("Number", value)
+	end):set_number_type(5, 30, false, 4)
+
+	self.properties_panel:add_left_right_selector("Array", "Hi", function(value)
+		print("Array", value)
+	end):set_array_type({ "hehe", "Hi", "My", "Name", "Is", "Decore", "..." }, true, 2)
+end
+
+---Draw entities page
+---@param context table
+---@param page_name string
+function M:draw_page_entities(context, page_name)
+	local entities = context
+	self.properties_panel.text_header:set_text(page_name .. " (" .. #entities .. ")")
+
+	for i = 1, #entities do
+		local entity = entities[i]
+		local entity_name = entity.id .. ". " .. (entity.prefab_id or "No Prefab")
+		self.properties_panel:add_button(entity_name, function()
+			self:select_page(PAGES.TABLE, entity, entity.prefab_id or "No Prefab")
+		end):set_text_button("Inspect")
+	end
+end
+
+---Draw systems page
+---@param context table
+---@param page_name string
+function M:draw_page_systems(context, page_name)
+	local systems = context
+	self.properties_panel.text_header:set_text(page_name .. "(" .. #systems .. ")")
+	for i = 1, #systems do
+		local system = systems[i]
+		local system_name = system.id .. " (" .. #system.entities .. ")"
+		self.properties_panel:add_button(system_name, function()
+			self:select_page(PAGES.SYSTEM, system, system.id)
+		end)
+	end
+end
+
+---Draw system details page
+---@param context table
+---@param page_name string
+function M:draw_page_system(context, page_name)
+	local system = context
+	self.properties_panel.text_header:set_text("System " .. page_name)
+
+	local entities = system.entities
+	for i = 1, #entities do
+		local entity = entities[i]
+		local entity_name = entity.id .. ". " .. (entity.prefab_id or "No Prefab")
+		self.properties_panel:add_button(entity_name, function()
+			self:select_page(PAGES.TABLE, entity, entity.prefab_id or "No Prefab")
+		end):set_text_button("Inspect")
+	end
+
+	local command_system_id = "command_" .. system.id
+	if self.world[command_system_id] then
+		self.properties_panel:add_button("Command", function()
+			self:select_page(PAGES.TABLE, self.world[command_system_id], command_system_id)
+		end):set_text_button("Inspect")
+	end
+end
+
+---Draw table page
+---@param context table
+---@param page_name string
+function M:draw_page_table(context, page_name)
+	local entity = context
+
+	local component_order = {}
+	for component_id in pairs(entity) do
+		table.insert(component_order, component_id)
+	end
+	table.sort(component_order, function(a, b)
+		local a_type = type(entity[a])
+		local b_type = type(entity[b])
+		if a_type ~= b_type then
+			return a_type < b_type
+		end
+		if type(a) == "number" and type(b) == "number" then
+			return a < b
+		end
+		return tostring(a) < tostring(b)
+	end)
+
+	for i = 1, #component_order do
+		local component_id = component_order[i]
+		local component = entity[component_id]
+		self:add_property_component(component_id, component, context)
+	end
+
+	local metatable = getmetatable(entity)
+	if metatable and metatable.__index then
+		for component_id, component in pairs(metatable.__index) do
+			self:add_property_component("M:" .. component_id, component, context)
+		end
+	end
+end
+
+---Draw entity prefabs page
+---@param context table
+---@param page_name string
+function M:draw_page_entity_prefabs(context, page_name)
+	for _, pack_id in ipairs(decore_data.entities_order) do
+		self.properties_panel:add_text("Pack id: " .. pack_id)
+
+		-- Sort
+		local entities_ordered = {}
+		for prefab_id in pairs(decore_data.entities[pack_id]) do
+			table.insert(entities_ordered, prefab_id)
+		end
+		table.sort(entities_ordered, function(a, b)
+			if type(a) == "number" and type(b) == "number" then
+				return a < b
+			end
+			return tostring(a) < tostring(b)
+		end)
+
+		for i = 1, #entities_ordered do
+			local prefab_id = entities_ordered[i]
+			if type(prefab_id) == "string" then
+				self.properties_panel:add_button(prefab_id, function()
+					self:select_page(PAGES.TABLE, decore_data.entities[pack_id][prefab_id], prefab_id)
+				end)
+			end
+		end
+	end
+end
+
 function M:select_page(page, context, page_name, is_going_back)
+	if not is_going_back then
+		self.undo_stack = {} -- This is undo for going back button, confusing a little?
+	end
 	self.properties_panel:clear()
 	self.context = context
 
@@ -50,122 +234,22 @@ function M:select_page(page, context, page_name, is_going_back)
 		table.insert(self.page_stack, { page, context, page_name })
 	end
 
-	-- Update prev page text
 	local preb_stack = self.page_stack[#self.page_stack - 1]
 	self.text_prev_page:set_text(preb_stack and preb_stack[3] or "")
 	self.properties_panel.text_header:set_text(page_name or "")
 
 	if page == PAGES.MAIN then
-		self.properties_panel:add_input("World Name", "Dreams", function(value)
-			print("World", value)
-		end)
-
-		self.properties_panel:add_button("Entities", function()
-			self:select_page(PAGES.ENTITIES, self.world.entities, "Entities")
-		end):set_text_button("Inspect")
-			:set_color("#A1D7F5")
-
-		self.properties_panel:add_button("Systems" , function()
-			self:select_page(PAGES.SYSTEMS, self.world.systems, "Systems")
-		end):set_text_button("Inspect")
-			:set_color("#E6DF9F")
-
-		self.properties_panel:add_button("Entity Prefabs" , function()
-			self:select_page(PAGES.ENTITY_PREFABS, nil, "Entity Prefabs")
-		end):set_text_button("Open")
-	end
-
-	if page == PAGES.ENTITIES then
-		local entities = context
-		self.properties_panel.text_header:set_text(page_name .. " (" .. #entities .. ")")
-
-		for i = 1, #entities do
-			local entity = entities[i]
-			local entity_name = entity.id .. ". " .. (entity.prefab_id or "No Prefab")
-			self.properties_panel:add_button(entity_name, function()
-				self:select_page(PAGES.ENTITY, entity, entity.prefab_id or "No Prefab")
-			end):set_text_button("Inspect")
-		end
-	end
-
-	if page == PAGES.SYSTEMS then
-		local systems = context
-		self.properties_panel.text_header:set_text(page_name .. "(" .. #systems .. ")")
-		for i = 1, #systems do
-			local system = systems[i]
-			local system_name = system.id .. " (" .. #system.entities .. ")"
-			self.properties_panel:add_button(system_name, function()
-				self:select_page(PAGES.SYSTEM, system, system.id)
-			end)
-		end
-	end
-
-	if page == PAGES.SYSTEM then
-		local system = context
-		self.properties_panel.text_header:set_text("System " .. page_name)
-
-		local entities = system.entities
-		for i = 1, #entities do
-			local entity = entities[i]
-			local entity_name = entity.id .. ". " .. (entity.prefab_id or "No Prefab")
-			self.properties_panel:add_button(entity_name, function()
-				self:select_page(PAGES.ENTITY, entity, entity.prefab_id or "No Prefab")
-			end):set_text_button("Inspect")
-		end
-
-		local command_system_id = "command_" .. system.id
-		if self.world[command_system_id] then
-			self.properties_panel:add_button("Command", function()
-				self:select_page(PAGES.ENTITY, self.world[command_system_id], command_system_id)
-			end):set_text_button("Inspect")
-		end
-	end
-
-	-- Should draw a table basically
-	if page == PAGES.ENTITY then
-		local entity = context
-
-		local component_order = {}
-		for component_id in pairs(entity) do
-			table.insert(component_order, component_id)
-		end
-		table.sort(component_order, function(a, b)
-			-- Sort by type first then by name
-			local a_type = type(entity[a])
-			local b_type = type(entity[b])
-			if a_type ~= b_type then
-				return a_type < b_type
-			end
-
-			return tostring(a) < tostring(b)
-		end)
-
-		for i = 1, #component_order do
-			local component_id = component_order[i]
-			local component = entity[component_id]
-			self:add_property_component(component_id, component, context)
-		end
-
-		-- Draw metatable values
-		local metatable = getmetatable(entity)
-		if metatable and metatable.__index then
-			for component_id, component in pairs(metatable.__index) do
-				self:add_property_component("M:" .. component_id, component, context)
-			end
-		end
-	end
-
-	if page == PAGES.ENTITY_PREFABS then
-		for _, pack_id in ipairs(decore_data.entities_order) do
-			self.properties_panel:add_text("Pack id: " .. pack_id)
-			for prefab_id, prefab_data in pairs(decore_data.entities[pack_id]) do
-				if type(prefab_id) == "string" then
-					self.properties_panel:add_button(prefab_id, function()
-						self:select_page(PAGES.ENTITY, prefab_data, prefab_id)
-					end)
-				end
-			end
-		end
+		self:draw_page_main(context, page_name)
+	elseif page == PAGES.ENTITIES then
+		self:draw_page_entities(context, page_name)
+	elseif page == PAGES.SYSTEMS then
+		self:draw_page_systems(context, page_name)
+	elseif page == PAGES.SYSTEM then
+		self:draw_page_system(context, page_name)
+	elseif page == PAGES.TABLE then
+		self:draw_page_table(context, page_name)
+	elseif page == PAGES.ENTITY_PREFABS then
+		self:draw_page_entity_prefabs(context, page_name)
 	end
 end
 
@@ -183,8 +267,18 @@ function M:add_property_component(component_id, component, context)
 		if is_array then
 			name = "Inspect (" .. #component .. ")"
 		end
-		self.properties_panel:add_button(component_id, function()
-			self:select_page(PAGES.ENTITY, component, component_id)
+
+		local button_name = component_id
+		-- If it's a number or array, try to get the id/name/prefab_id from the component
+		if type(component) == "table" and type(component_id) == "number" then
+			local extracted_id = component.id or component.name or component.prefab_id or component.node_id
+			if extracted_id then
+				button_name = component_id .. ". " .. extracted_id
+			end
+		end
+
+		self.properties_panel:add_button(button_name, function()
+			self:select_page(PAGES.TABLE, component, component_id)
 		end):set_text_button(name)
 	end
 
